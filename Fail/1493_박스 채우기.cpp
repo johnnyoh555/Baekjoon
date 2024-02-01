270faee74c90e8abed099a836adf3eebfac8c41e
Jonggeun Oh <jooh@student.42.fr>
2024-02-01 13:08:29 +0900

    add folder

---
+++ Fail/1493_박스 채우기.cpp
@@ -0,0 +1,17 @@
+// https://www.acmicpc.net/problem/1493
+
+#include <iostream>
+#include <vector>
+
+int main() {
+	int l;
+	int w;
+	int h;
+	int	N;
+
+	std::cin >> l >> w >> h >> N;
+	
+	for (int i = 0; i < N; i++) {
+
+	}
+}
