4e462427cb95b074224de6936e5652c6b649a320
jooh <johnn55@naver.com>
2024-03-09 16:33:02 +0900

    2011 refactor

--- 24.02/2011_암호코드.cpp
+++ 24.02/2011_암호코드.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 int d[5001];
 int mod = 1000000;
+string str;
 
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  std::string str;
   cin >> str;
   if (str[0] == '0') {
     cout << "0\n";
@@ -21,20 +21,10 @@ int main(void) {
   if (str[1] != '0') d[2]++;
   int tmp = 10 * (str[0] - '0') + str[1] - '0';
   if (10 <= tmp && tmp <= 26) d[2]++;
-  if (!d[2]) {
-    cout << "0\n";
-    return 0;
-  }
   for (int i = 3; i <= str.size(); i++) {
     if (str[i - 1] != '0') d[i] = (d[i] + d[i - 1]) % mod;
     int t = 10 * (str[i - 2] - '0') + str[i - 1] - '0';
-    if (t >= 10 && t <= 26) {
-      d[i] = (d[i] + d[i - 2]) % mod;
-    }
-    if (!d[i]) {
-      cout << "0\n";
-      return 0;
-    }
+    if (t >= 10 && t <= 26) d[i] = (d[i] + d[i - 2]) % mod;
   }
   cout << d[str.size()] << '\n';
 }
