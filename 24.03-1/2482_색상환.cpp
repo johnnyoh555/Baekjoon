5e35da49ba6ad890f827a586edddea53aa53063e
OJG <johnn55@naver.com>
2024-03-09 13:34:56 +0900

    2482

--- 24.03-1/2482_색상환.cpp
+++ 24.03-1/2482_색상환.cpp
@@ -4,15 +4,23 @@
 
 using namespace std;
 
-int arr[1001][1001];
-int N, K;
+int d[1001][1001];
+int N, K, sum, mod = 1000000003;
 
 int	main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
 	cin >> N >> K;
-	for (int i = 1; i <= K; i++) {
-		for (int j = 1; j <= N; i++)
+	for (int i = 0; i < N; i++) {
+	 d[i][0] = 1;
+	 d[i][1] = i;
 	}
+	for (int i = 2; i <= N; i++) {
+		for (int j = 2; j <= K; j++) {
+			d[i][j] = (d[i - 2][j - 1] + d[i - 1][j]) % mod;
+		}
+	}
+	cout << (d[N - 1][K] + d[N - 3][K - 1]) % mod << '\n';
 }
+
