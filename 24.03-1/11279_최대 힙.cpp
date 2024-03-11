c9c3e247b4b12290248dbaac7d95b183f2b2f6b6
jooh <johnn55@naver.com>
2024-03-11 20:03:00 +0900

    1927/11279/11286

---
+++ 24.03-1/11279_최대 힙.cpp
@@ -0,0 +1,34 @@
+// https://www.acmicpc.net/problem/11279
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+struct cmp {
+	bool operator()(int a, int b) {
+		return a < b;
+	};
+};
+
+priority_queue<int, vector<int>, cmp> pq;
+int N;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N;
+	for (int i = 0; i < N; i++ ) {
+		int tmp;
+		cin >> tmp;
+		if (tmp != 0) {
+			pq.push(tmp);
+		} else if(pq.empty()) {
+			cout << "0\n";
+		} else {
+			cout << pq.top() << '\n';
+			pq.pop();
+		}
+	}
+}
---
+++ 24.03-1/11286_절대값 힙.cpp
@@ -0,0 +1,36 @@
+// https://www.acmicpc.net/problem/11286
+
+#include <iostream>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
+struct cmp {
+	bool operator()(int a, int b) {
+		if (abs(a) == abs(b)) return a > b;
+		return abs(a) > abs(b);
+	};
+};
+
+priority_queue<int, vector<int>, cmp> pq;
+int N;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N;
+	for (int i = 0; i < N; i++ ) {
+		int tmp;
+		cin >> tmp;
+		if (tmp != 0) {
+			pq.push(tmp);
+		} else if(pq.empty()) {
+			cout << "0\n";
+		} else {
+			cout << pq.top() << '\n';
+			pq.pop();
+		}
+	}
+}
