9b48d0b25394085733dc52e4649fa4a5a9e00a89
jooh <johnn55@naver.com>
2024-10-07 16:47:31 +0900

    24.10.07

--- 24.10/2602_돌다리 건너기.cpp
+++ 24.10/2602_돌다리 건너기.cpp
@@ -11,13 +11,11 @@ int main() {
   cin >> str >> b[0] >> b[1];
   int ss = str.size(), bb = b[0].size();
   for (int i = 0; i < bb; i++) {dp[0][0][i] = 1; dp[1][0][i] = 1;}
-  for (int i = 0; i < ss; i++) {
-    for (int j = 0; j < 2; j++) {
+  for (int i = 0; i < ss; i++)
+    for (int j = 0; j < 2; j++)
       for (int k = 0; k < bb; k++) {
         if (b[j][k] == str[i]) dp[j][i + 1][k + 1] += dp[(j + 1) % 2][i][k];
         dp[j][i + 1][k + 1] += dp[j][i + 1][k];
       }
-    }
-  }
   cout << dp[0][ss][bb] + dp[1][ss][bb] << '\n';
 }
