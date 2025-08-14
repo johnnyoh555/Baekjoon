67d4b57cc14dc23b470c5a81aad2f8f69bb2b4d8
jooh <johnn55@naver.com>
2025-08-14 15:49:29 +0900

    25345

--- 25.08/25345_루나의 게임 세팅.cpp
+++ 25.08/25345_루나의 게임 세팅.cpp
@@ -1,25 +1,18 @@
 // https://www.acmicpc.net/problem/25345
 #include <iostream>
 using namespace std;
-long long cnt, MOD =  1e9 + 7, N, K, d[2001][2001], com;
+long long M = 1e9 + 7, N, K, d[2001][2001], a;
 
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
   cin >> N >> K;
-  for (int i = 0; i < N; i++) {
-    long long tmp; cin >> tmp;
-  }
+  for (int i = 0; i < N; i++) cin >> a;
+  for (int i = 0; i <= N; i++) d[i][0] = 1;
   for (int i = 1; i <= N; i++)
-    d[i][1] = 1;
-  for (int j = 2; j <= K; j++)
-    for (int i = j; i <= N; i++)
-      d[i][j] = (d[i - 1][j] + d[i - 1][j - 1]) % MOD;
-  for (int i = 1; i <= N; i++) com += d[i][K];
-  for (int i = 1; i < K; i++) {
-    com *= 2;
-    com %= MOD;
-  }
-  cout << com << '\n';
+    for (int j = 1; j <= i; j++)
+      d[i][j] = (d[i - 1][j] + d[i - 1][j - 1]) % M;
+  for (int i = 1; i < K; i++) {d[N][K] *= 2; d[N][K] %= M;}
+  cout << d[N][K] << '\n';
 }
