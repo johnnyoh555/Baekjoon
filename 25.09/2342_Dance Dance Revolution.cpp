a9acd580f91f35a5903dba6bce5facd96b29ba78
jooh <johnn55@naver.com>
2025-09-19 12:46:10 +0900

    2342

--- 25.09/2342_Dance Dance Revolution.cpp
+++ 25.09/2342_Dance Dance Revolution.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int dp[5][5], ans = 2e9;
+int dp[5][5], ndp[5][5], ans;
+const int INF = 2e9;
 
 int ft(int from, int to) {
   if (from == 0) return 2;
@@ -15,25 +16,23 @@ int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
   
-  fill(&dp[0][0], &dp[4][5], 2e9);
+  ans = INF;
+  fill(&dp[0][0], &dp[4][5], INF);
   dp[0][0] = 0;
   while (true) {
     int tmp; cin >> tmp;
     if (!tmp) break;
-    int ndp[5][5];
-    fill(&ndp[0][0], &ndp[4][5], 2e9);
+    fill(&ndp[0][0], &ndp[4][5], INF);
     for (int i = 0; i < 5; i++) {
       for (int j = 0; j < 5; j++) {
-        if (dp[i][j] == 2e9) continue;
+        if (dp[i][j] == INF) continue;
         if (i != tmp)
           ndp[i][tmp] = min(ndp[i][tmp], dp[i][j] + ft(j, tmp));
         if (j != tmp)
           ndp[tmp][j] = min(ndp[tmp][j], dp[i][j] + ft(i, tmp));
       }
     }
-    for (int i = 0; i < 5; i++)
-      for (int j = 0; j < 5; j++)
-        dp[i][j] = ndp[i][j];
+    swap(dp, ndp);
   }
   for (int i = 0; i < 5; i++)
     for (int j = 0; j < 5; j++)
